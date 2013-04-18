caba68122889784b7c4be6ddb17d06066b19c9e5
gabriel <gabrielortiz_26@hotmail.com>
2013-04-17 20:31:12 -0600

    Creado nuevo proyecto

---
+++ build/CMakeFiles/xonix.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gabriel/projects/xonix/main.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/main.o"
+  "/home/gabriel/projects/xonix/src/enemigo.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/src/enemigo.o"
+  "/home/gabriel/projects/xonix/src/personaje.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/src/personaje.o"
+  "/home/gabriel/projects/xonix/src/poligono.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/src/poligono.o"
+  "/home/gabriel/projects/xonix/src/timer.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/src/timer.o"
+  "/home/gabriel/projects/xonix/src/xonix.cpp" "/home/gabriel/projects/xonix/build/CMakeFiles/xonix.dir/src/xonix.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/xonix.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/xonix.dir/src/enemigo.o"
+  "CMakeFiles/xonix.dir/src/poligono.o"
+  "CMakeFiles/xonix.dir/src/timer.o"
+  "CMakeFiles/xonix.dir/src/personaje.o"
+  "CMakeFiles/xonix.dir/main.o"
+  "CMakeFiles/xonix.dir/src/xonix.o"
+  "xonix.pdb"
+  "xonix"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/xonix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
